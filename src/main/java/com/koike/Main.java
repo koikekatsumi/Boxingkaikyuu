dd4f9a5c390da6a5e906796c29ea3b2f77ab2b23
koikekatsumi <katsumikoike724@gmail.com>
2024-03-18 23:36:08 +0900

    BoxingData→Boxerに変更

--- src/main/java/com/koike/Main.java
+++ src/main/java/com/koike/Main.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        BoxingData boxing1 = new BoxingData("寺田拳四朗", "ライトフライ級", 6);
-        BoxingData boxing2 = new BoxingData("山中慎介", "バンダム級", 12);
-        BoxingData boxing3 = new BoxingData("長谷川穂積", "バンダム級", 11);
-        BoxingData boxing4 = new BoxingData("畑山隆則", "スーパーフェザー級", 1);
-        BoxingData boxing5 = new BoxingData("辰吉丈一郎", "バンダム級", 3);
-        BoxingData boxing6 = new BoxingData("井上尚弥", "スーパーバンダム級", 1);
+        Boxer boxing1 = new Boxer("寺田拳四朗", "ライトフライ級", 6);
+        Boxer boxing2 = new Boxer("山中慎介", "バンダム級", 12);
+        Boxer boxing3 = new Boxer("長谷川穂積", "バンダム級", 11);
+        Boxer boxing4 = new Boxer("畑山隆則", "スーパーフェザー級", 1);
+        Boxer boxing5 = new Boxer("辰吉丈一郎", "バンダム級", 3);
+        Boxer boxing6 = new Boxer("井上尚弥", "スーパーバンダム級", 1);
 
-        List<BoxingData> boxingList = new ArrayList<>();
+        List<Boxer> boxingList = new ArrayList<>();
         boxingList.add(boxing1);
         boxingList.add(boxing2);
         boxingList.add(boxing3);
@@ -20,7 +20,7 @@ public class Main {
         boxingList.add(boxing5);
         boxingList.add(boxing6);
 
-        for (BoxingData boxing : boxingList) {
+        for (Boxer boxing : boxingList) {
             if ((boxing.getWeight().equals("バンダム級")) && (boxing.getDefense() > 10)) {
                 System.out.println(boxing.getName() + "は、スーパー王者です");
             } else if (boxing.getWeight().equals("スーパーフェザー級")) {
